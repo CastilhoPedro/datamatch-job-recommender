0af930dd270235cc3958e646a89f5c907a871740
CastilhoPedro <166401486+CastilhoPedro@users.noreply.github.com>
2025-10-07 13:35:22 -0300

    Add files via upload

--- src/backend/database/models.py
+++ src/backend/database/models.py
@@ -1,5 +1,5 @@
 # Em models.py
-from sqlalchemy import create_engine, Column, Integer, String, Text, Float, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy import create_engine, Column, Integer, String, Text, Float, DateTime, ForeignKey, UniqueConstraint, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.dialects.postgresql import JSONB
 import datetime
@@ -8,23 +8,45 @@ Base = declarative_base()
 
 class Vocabulario(Base):
     __tablename__ = 'vocabulario'
-    id_palavra = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     palavra = Column(Text, nullable=False, unique=True)
 
-class Vaga(Base):
-    __tablename__ = 'vagas'
-    id_vaga = Column(Integer, primary_key=True)
-    portal = Column(Text, nullable=False)
-    url = Column(Text, unique=True, nullable=False)
-    descricao = Column(Text)
-    data_coleta = Column(DateTime(timezone=True), default=datetime.datetime.now)
+class Localizacao(Base):
+    __tablename__ = 'localizacao'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    uf = Column(String(2), unique=True, nullable=False)
+
+class Senioridade(Base):
+    __tablename__ = 'senioridade'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nivel = Column(String(15), unique=True, nullable=False)
 
 class Vetores(Base):
     __tablename__ = 'vetores'
     id_vaga = Column(Integer, ForeignKey('vagas.id_vaga', ondelete='CASCADE'), primary_key=True)
-    id_palavra = Column(Integer, ForeignKey('vocabulario.id_palavra', ondelete='CASCADE'), primary_key=True)
+    id_palavra = Column(Integer, ForeignKey('vocabulario.id', ondelete='CASCADE'), primary_key=True)
     score_tfidf = Column(Float, nullable=False)
 
-    # Relacionamentos (opcional, mas bom para ORM)
-    vaga = relationship("Vaga")
+    
+    vaga = relationship("VagasRaw")
     vocabulario = relationship("Vocabulario")
+
+class VagasRaw(Base):
+    __tablename__ = 'vagas_raw'
+    id_vaga = Column(Integer, primary_key=True)
+    nome_vaga = Column(String(70), nullable=False)
+    descricao_vaga = Column(Text, nullable=False)
+    nome_empresa = Column(String(50))
+    localizacao = Column(String(2)) #aqui ele ainda vai trazer a UF, na processed que vamos transformar a UF em número para deixar mais barato computacionalmente.
+    data_publicacao = Column(String(15)) 
+    processado = Column(Boolean, default=False)
+    url = Column(Text, unique=True, nullable=False)
+    data_coleta = Column(DateTime(timezone=True), default=datetime.datetime.utcnow)
+
+class VagasProcessed(Base):
+    id_vaga = Column(Integer, ForeignKey('vagas_raw.id_vaga'), primary_key=True)
+    nome_vaga = Column(String(70), nullable=False)
+    nome_empresa = Column(String(50))
+    senioridade = Column(Integer, ForeignKey('senioridade.id'))
+    localizacao = Column(Integer, ForeignKey('localizacao.id'))
+    data_publicacao = Column(DateTime(timezone=True))
